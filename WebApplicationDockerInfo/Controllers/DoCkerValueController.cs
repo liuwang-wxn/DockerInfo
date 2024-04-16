a7ca9491e4d4183696c9e5e235f2cfff3d50283e
liliu314 <3116544996@qq.com>
2024-04-16 11:32:47 +0800

    13145

---
+++ WebApplicationDockerInfo/Controllers/DoCkerValueController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationDockerInfo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoCkerValueController : ControllerBase
+    {
+        [HttpGet]
+        public string GetValue()
+        {
+            return "1345678";
+        }
+    }
+}
